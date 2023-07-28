da912ede019a27ba3bbb5e879c5f51d82dbaf323
faizan2301 <skfaizan2301@gmail.com>
2023-07-28 11:57:37 +0530

    Some Changes

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const { Server } = require("ws");
 app.get("/", (req, res) => {
   res.send("Node Server is running. Yay!!");
 });
-const socketio = require("socket.io")(http);
+// const socketio = require("socket.io")(http);
+const wss = new Server({ server });
+wss.on("connection", function (ws, req) {
+  ws.on("message", (message) => {
+    var dataString = message.toString();
+  });
+});
 
-socketio.on("connection", (userSocket) => {
-  userSocket.on("send_message", (data) => {
-    userSocket.broadcast.emit("receive_message", data);
+var webSockets = {};
+
+wss.on("connection", function (ws, req) {
+  var userID = req.url.substr(1); //get userid from URL/userid
+  webSockets[userID] = ws; //add new user to the connection list
+  console.log("User connected:", userID);
+  ws.on("message", (message) => {
+    // If there is any message
+    var datastring = message.toString();
+    if (datastring.charAt(0) == "{") {
+      // Check if message starts with '{' to check if it's json
+      datastring = datastring.replace(/\'/g, '"');
+      var data = JSON.parse(datastring);
+      if (data.auth == "chatappauthkey231r4") {
+        var boardws = webSockets[data.userid]; // Send to connected user
+        var msg =
+          "{'cmd':'message','msg':'" +
+          data.msg +
+          "','from':'" +
+          data.from +
+          "'}";
+        boardws.send(msg);
+      }
+    }
   });
 });
 http.listen(8080);
